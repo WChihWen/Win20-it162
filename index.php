825617e632d2663374c75a6785fb316e17c2941d
WChihWen <65738834+WChihWen@users.noreply.github.com>
2021-01-25 08:34:43 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include 'includes/header.php';?>  
+
+   <div class="wrapper">
+      <section>   
+         <img class="desktop" src="images/desktop.jpg" alt="Chih Wen Wang" />       
+         <img class="tablet" src="images/tablet.jpg" alt="Space Needle of Seattle" />       
+         <img class="phone" src="images/phone.jpg" alt="Chih Wen Wang" />       
+         <h2 class="subheader">A little about me!</h2>      
+         <p>
+            &nbsp;&nbsp;My name is Chih Wen Wang, and I come from Taiwan. When I was a university student in Taiwan, I studied information technology and management. However, I focused on learning how to build software and let it work on a computer or the Internet. While graduating, I became a programmer at a technology company in my country and worked for almost ten years. 
+         </p>
+         <p>
+            &nbsp;&nbsp;However, my life was changed because I came to the United States with my wife. I have two goals now. One is that I have to improve my English ability. So, I go to study the ESL Program here. Another goal is that I yearn to earn an IT Program degree or certificate in the United States because I do not have any degree in the United States. In addition, I believe that I can learn more things from you guys.
+         </p>
+      </section>
+      <aside>
+         <h1>Information</h1>
+         <a href="https://github.com/WChihWen" target="blank">Chih Wen Wang's GitHub</a>
+      </aside>
+   </div>
+<?php include 'includes/footer.php';?>
+    
